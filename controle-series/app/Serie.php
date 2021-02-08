0401d78001af142fa1e202657e382869c9c7fe18
MattheusHenrique <58952630+MattheusHenrique@users.noreply.github.com>
2021-02-08 11:37:19 -0300

    Criando banco de dados

--- controle-series/app/Serie.php
+++ controle-series/app/Serie.php
@@ -9,4 +9,9 @@ class Serie extends Model
     public $timestamps = false;
     protected $fillable = ['nome', 'id'];
 
+    public function temporadas() 
+    {
+        return $this->hasMany(Temporada::class);
+    }
+
 }
